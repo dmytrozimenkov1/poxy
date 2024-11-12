639a1b23aa39535870750889ea6faa04e5ebe2db
Dmytro Zimenkov <dmytro.zimenkov@gmail.com>
2024-11-12 23:49:33 +0100

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/poxy.gif" alt="Poxy Studio" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
