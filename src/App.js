030b9472acaa2f2a2a8135baa076f0beeee6c0e3
Dmytro Zimenkov <dmytro.zimenkov@gmail.com>
2024-11-14 20:03:10 +0100

    added buro

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const isBuro = window.location.hostname === 'buro.poxy.studio';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/poxy.gif" alt="Poxy Studio" />
+        {isBuro ? (
+          <h1>Hello World</h1>
+        ) : (
+          <img src="/poxy.gif" alt="Poxy Studio" />
+        )}
       </header>
     </div>
   );
